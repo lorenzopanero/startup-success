1cee9f6cdc13e4af055364390dd45e8814ed8a05
valebonato <bonatovalentina99@gmail.com>
2025-03-12 15:32:04 +0100

    adding some images

---
+++ src/ingestion.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import requests
+import json # Open and read the JSON file 
+
+# Funzione import csv
+def download_from_drive(csv_name):
+    with open("data/file_id.json", "r", encoding="utf-8") as f: 
+        data = json.load(f) # Print the data 
+    file_id = data[csv_name]
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    response = requests.get(url)
+    with open("file.csv", "wb") as f:
+        f.write(response.content)
+    return pd.read_csv("file.csv")
+
+# Funzione import json palette
+def import_palette_json(file_path):
+    with open(file_path, 'r') as file:
+        palette = json.load(file)
+    return palette
